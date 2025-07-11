1ff66daf9ff0ce70c439bad5159f620c7656b57b
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-11 17:36:26 +0300

    create home page route

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:theker_app/core/helper_function.dart';
 import 'package:theker_app/views/home_view.dart';
-import 'package:theker_app/views/splash_view.dart';
 
 void main() {
   runApp(const ThkerApp());
@@ -14,9 +13,8 @@ class ThkerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // onGenerateRoute: onGenerateRoute,
-      // initialRoute: SplashView.routeName,
-      home: HomeView(),
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: HomeView.routeName,
     );
   }
 }
