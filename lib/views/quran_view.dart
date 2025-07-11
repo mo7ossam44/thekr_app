24388df2ffbf7b86b673fcd3b17bc52441fdd63f
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-11 17:38:13 +0300

    add quran view

--- lib/views/quran_view.dart
+++ lib/views/quran_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:theker_app/widgets/custom_text_filed.dart';
+import 'package:theker_app/widgets/quran_view_body.dart';
 
 class QuranView extends StatelessWidget {
   const QuranView({super.key});
 
+  static const String routeName = '/quran';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(color: Colors.white),
+        centerTitle: true,
+        title: CustomTextFiledAppBar(),
+      ),
+      backgroundColor: const Color(0xff1C1A1A),
+      body: const SafeArea(child: QuranViewBody()),
+    );
   }
 }
