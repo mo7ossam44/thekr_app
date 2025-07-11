4b5287ffc21b0db4696ca3eb74be73717ea88bf3
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-11 17:38:01 +0300

    add home view route name

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -4,6 +4,8 @@ import 'package:theker_app/widgets/home_view_body.dart';
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
+  static const String routeName = 'home';
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
