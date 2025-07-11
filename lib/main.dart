c7f9cc839f12dcd7738cdfdaf4c271fb234a6e9c
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-11 14:21:32 +0300

    start in home page instead

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:theker_app/core/helper_function.dart';
+import 'package:theker_app/views/home_view.dart';
 import 'package:theker_app/views/splash_view.dart';
 
 void main() {
@@ -13,9 +14,9 @@ class ThkerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      onGenerateRoute: onGenerateRoute,
-      initialRoute: SplashView.routeName,
-      // home: HomePage(),
+      // onGenerateRoute: onGenerateRoute,
+      // initialRoute: SplashView.routeName,
+      home: HomeView(),
     );
   }
 }
