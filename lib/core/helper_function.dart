d65c9cfa86c7530894f262cc5a742a7a8eceb830
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-11 17:37:22 +0300

    add new pages routes

--- lib/core/helper_function.dart
+++ lib/core/helper_function.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:theker_app/views/home_view.dart';
 import 'package:theker_app/views/on_boarding_view.dart';
+import 'package:theker_app/views/quran_view.dart';
 import 'package:theker_app/views/splash_view.dart';
+import 'package:theker_app/views/surah_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
@@ -9,6 +12,12 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const SplashView());
     case OnBoardingView.routeName:
       return MaterialPageRoute(builder: (context) => const OnBoardingView());
+    case QuranView.routeName:
+      return MaterialPageRoute(builder: (context) => const QuranView());
+    case HomeView.routeName:
+      return MaterialPageRoute(builder: (context) => const HomeView());
+    case SurahView.routeName:
+      return MaterialPageRoute(builder: (context) => const SurahView());
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
