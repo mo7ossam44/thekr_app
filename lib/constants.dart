b3427423d8e609803e25f768b81c62cd80dec2d7
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-17 14:43:47 +0300

    add constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'dart:ui';
 const kPrimaryColor = Color(0xff007D45);
 const kPrimaryFont = 'Marhey';
 const kSecondaryFont = 'Tajawal';
+const kFajrHadth =  'قال رسول الله صلى الله عليه وسلم: (مَن صَلَّى الصُّبْحَ فَهو في ذِمَّةِ اللهِ، فلا يَطْلُبَنَّكُمُ اللَّهُ مِن ذِمَّتِهِ بشيءٍ فيُدْرِكَهُ فَيَكُبَّهُ في نَارِ جَهَنَّمَ).';
