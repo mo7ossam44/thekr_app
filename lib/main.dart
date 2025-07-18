e30854e4fd26dea968cfebb6e7df5f9d56989772
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-18 16:49:41 +0300

    edit the cursror color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:theker_app/core/helper_function.dart';
 import 'package:theker_app/views/home_view.dart';
+// import 'package:theker_app/views/prayer_time_view.dart';
 
 void main() {
   runApp(const ThkerApp());
@@ -12,6 +13,13 @@ class ThkerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        
+        brightness: Brightness.dark,
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor: Colors.white.withOpacity(0.4),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
       initialRoute: HomeView.routeName,
