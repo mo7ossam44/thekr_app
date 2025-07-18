5643c03725bf5639aa69ac9d79656c883565b396
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-18 16:50:05 +0300

    add sliver to box adapter to the page

--- lib/widgets/app_bar_home_view.dart
+++ lib/widgets/app_bar_home_view.dart
@@ -8,32 +8,37 @@ class CustomAppBarHomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Icon(Icons.settings, size: 20, color: Colors.white),
-            SizedBox(width: 5),
-            Icon(Icons.search_outlined, size: 20, color: Colors.white),
-            SizedBox(width: 5),
-            Icon(Icons.star_border_sharp, size: 20, color: Colors.white),
+            Row(
+              children: [
+                Icon(Icons.settings, size: 20, color: Colors.white),
+                SizedBox(width: 5),
+                Icon(Icons.search_outlined, size: 20, color: Colors.white),
+                SizedBox(width: 5),
+                Icon(Icons.star_border_sharp, size: 20, color: Colors.white),
+              ],
+            ),
+            Text(
+              'ذِكر',
+              style: TextStyle(
+                fontFamily: kPrimaryFont,
+                fontSize: 25,
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.play_arrow_rounded, color: Colors.white),
+            ),
           ],
         ),
-        Text(
-          'ذِكر',
-          style: TextStyle(
-            fontFamily: kPrimaryFont,
-            fontSize: 25,
-            fontWeight: FontWeight.w400,
-            color: Colors.white,
-          ),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.play_arrow_rounded, color: Colors.white),
-        ),
-      ],
+      ),
     );
   }
 }
