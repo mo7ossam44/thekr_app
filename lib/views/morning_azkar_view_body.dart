fce3268f48f5b75556303144c6fd950c2255842d
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-18 16:44:09 +0300

    keep the code more clean

--- lib/views/morning_azkar_view_body.dart
+++ lib/views/morning_azkar_view_body.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:theker_app/cubits/get_morning_azkar/get_morning_azkar_cubit.dart';
 import 'package:theker_app/cubits/get_morning_azkar/get_morning_azkar_states.dart';
-import 'package:theker_app/models/morning_azkar_model.dart';
-import 'package:theker_app/services/morning_azkar_service.dart';
-import 'package:theker_app/widgets/custom_azkar_container.dart';
+import 'package:theker_app/widgets/morning_azkar_list_view.dart';
 
 class MorningAzkarViewBody extends StatefulWidget {
   const MorningAzkarViewBody({super.key});
@@ -30,12 +28,7 @@ class _MorningAzkarViewBodyState extends State<MorningAzkarViewBody> {
       builder: (context, state) {
         if (state is MorningAzkarSucess) {
           var data = BlocProvider.of<GetMorningAzkarCubit>(context).getData();
-          return ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return CustomAzkarContainerCard(morningAzkar: data[index]);
-            },
-          );
+          return MorningAzkarListViewBuilder(data: data);
         } else if (state is MorningAzkarFaliure) {
           return Center(child: Text('No Morning Azkar Now'));
         } else {
