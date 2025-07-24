fcc0454c533c2e5016be6a73850c4fe28816752b
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-24 20:00:30 +0300

    remove unn pathes

--- lib/widgets/views_body/quran_view_body.dart
+++ lib/widgets/views_body/quran_view_body.dart
@@ -3,10 +3,10 @@ import 'package:theker_app/constants.dart';
 import 'package:theker_app/models/surah_model.dart';
 import 'package:theker_app/services/surah_service.dart';
 import 'package:theker_app/widgets/custom_quran_categories_container.dart';
-import 'package:theker_app/widgets/custom_text_filed.dart';
+import 'package:theker_app/widgets/surah_view_parts/custom_text_filed.dart';
 import 'package:theker_app/widgets/ketma_container_widget.dart';
 import 'package:theker_app/widgets/sliver_hight_widget.dart';
-import 'package:theker_app/widgets/surah_grid_view_builder.dart';
+import 'package:theker_app/widgets/surah_view_parts/surah_grid_view_builder.dart';
 
 class QuranViewBody extends StatefulWidget {
   const QuranViewBody({super.key});
