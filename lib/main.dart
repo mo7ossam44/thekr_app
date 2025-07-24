36ba7b7755a1ae5393adc4097c92dd921c5d4f77
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-24 19:59:39 +0300

    add not accept list in constants file and download vector from figma and remove unn pathes from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'package:theker_app/constants.dart';
-import 'package:theker_app/core/helper_function.dart';
-import 'package:theker_app/cubits/add_zeker_cubit/add_zeker_cubit.dart';
-import 'package:theker_app/cubits/get_azkar_cubit/get_azkar_cubit.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:theker_app/cubits/zeker_cubit/zeker_cubit.dart';
-import 'package:theker_app/models/zeker_model.dart';
-import 'package:theker_app/simple_bloc_observer.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:theker_app/views/home_view.dart';
-import 'package:theker_app/views/splash_view.dart';
+import 'package:theker_app/models/zeker_model.dart';
+import 'package:theker_app/core/helper_function.dart';
+import 'package:theker_app/simple_bloc_observer.dart';
+import 'package:theker_app/cubits/zeker_cubit/zeker_cubit.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:theker_app/cubits/get_azkar_cubit/get_azkar_cubit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
